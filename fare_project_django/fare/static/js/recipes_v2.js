55b71e9148be89e08adf99db3a849925bbaf10fb
fadykalo <fadykalo@gmail.com>
2014-03-30 20:16:35 +0100

    minor changes in the swiping page

--- fare_project_django/fare/static/js/recipes_v2.js
+++ fare_project_django/fare/static/js/recipes_v2.js
@@ -13,7 +13,6 @@ function get_content(type) {
 
 function load_carousel(content, type) {
     var carousel = $("#carousel-" + type).find('.carousel-inner');
-    console.log(content);
 //    var listIndicators = $("#carousel-" + type + " ol.carousel-indicators");
     var i = 1;
     $.each(content, function (k, v) {
@@ -25,18 +24,19 @@ function load_carousel(content, type) {
                     )
                 ).append(
                         $('<div>',{class:"col-xs-8 item-info"}).append($('<div>', {}).html(k))
+
                     )
         )
         .append( $('<div>',{class:"row"}).append(
-                $('<div>',{class:"col-xs-6"}).append(
+                $('<div>',{class:"col-xs-5"}).append(
                     $('<div>',{class:"rating"}).append(
 
                     )
                 )
-            ).append($('<div>',{class:"col-xs-6"}).append( $('<div>',{class:"btn-group btn-group-xs pull-right"}
+            ).append($('<div>',{class:"col-xs-7"}).append( $('<div>',{class:"btn-group btn-group-xs pull-right"}
                 ).append(
                     $('<a>',{class:"btn btn-primary",href :'#' }).append(
-                        $('<span>',{class:'fui-time'}))
+                        $('<span>',{class:'fui-time'}).html(' '+v[2]/60 + 'min'))
                 ).append(
                     $('<a>',{class:"btn btn-primary",href :'#' }).append(
                         $('<span>',{class:'fui-check'}))
@@ -51,7 +51,25 @@ function load_carousel(content, type) {
                 )
             )
         ).appendTo(carousel);
-//        $('<li>').attr('data-slide-to',i).attr('data-target', '#carousel-' + type).appendTo(listIndicators);
+
+        var rating = carousel.find('.item').last().find('.rating'); console.log(rating);
+        for(j=1;j<6;j++){
+            var rank = $('<span>', {class: 'glyphicon'});
+            if (j <= v[3]) {
+                $(rating).append(rank.addClass('glyphicon-star'));
+            } else {
+                $(rating).append(rank.addClass('glyphicon-star-empty'));
+            }
+        }
+        var arr = v[4].cuisine;
+        if(typeof arr != 'undefined'){
+            var info = carousel.find('.item').last().find('.item-info'); console.log(info);
+            for(j=0;j<arr.length;j++){
+                var label = $('<span>', {class: 'label'});
+                    $(info).append(label.html(arr[j]));
+            }
+        }
+
         ++i;
     });
 }
