a01194429012bff8f7c40edab4df00ceec6a76d9
fadykalo <fadykalo@gmail.com>
2014-03-30 18:38:00 +0100

    added new layout for recipes and css

--- fare_project_django/fare/static/js/recipes_v2.js
+++ fare_project_django/fare/static/js/recipes_v2.js
@@ -12,19 +12,46 @@ function get_content(type) {
 }
 
 function load_carousel(content, type) {
-    var carousel = $("#carousel-" + type).find('.carousel-inner'); console.log(carousel);
-    var listIndicators = $("#carousel-" + type + " ol.carousel-indicators");
+    var carousel = $("#carousel-" + type).find('.carousel-inner');
+    console.log(content);
+//    var listIndicators = $("#carousel-" + type + " ol.carousel-indicators");
     var i = 1;
     $.each(content, function (k, v) {
         $('<div>', {class: 'item'})
         .append(
-            $('<img/>', {class: 'img-responsive', src: v[0]})
+                $('<div>',{class:'row pad-top-20'}).append(
+                    $('<div>',{class:"col-xs-4 text-center recipe_pix"}).append(
+                        $('<img/>', {class: 'img-circle', src: v[0][0]})
+                    )
+                ).append(
+                        $('<div>',{class:"col-xs-8 item-info"}).append($('<div>', {}).html(k))
+                    )
         )
-        .append(
-            $('<div>', {class: 'carousel-caption img-overlay-label'})
-                .append($('<span>', {class: 'text'}).html(k))
+        .append( $('<div>',{class:"row"}).append(
+                $('<div>',{class:"col-xs-6"}).append(
+                    $('<div>',{class:"rating"}).append(
+
+                    )
+                )
+            ).append($('<div>',{class:"col-xs-6"}).append( $('<div>',{class:"btn-group btn-group-xs pull-right"}
+                ).append(
+                    $('<a>',{class:"btn btn-primary",href :'#' }).append(
+                        $('<span>',{class:'fui-time'}))
+                ).append(
+                    $('<a>',{class:"btn btn-primary",href :'#' }).append(
+                        $('<span>',{class:'fui-check'}))
+                ).append(
+                    $('<a>',{class:"btn btn-primary",href :'#' }).append(
+                        $('<span>',{class:'fui-heart'}))
+                ).append(
+                    $('<a>',{class:"btn btn-primary",href :'#' }).append(
+                        $('<span>',{class:'fui-eye'}))
+                )
+
+                )
+            )
         ).appendTo(carousel);
-        $('<li>').attr('data-slide-to',i).attr('data-target', '#carousel-' + type).appendTo(listIndicators);
+//        $('<li>').attr('data-slide-to',i).attr('data-target', '#carousel-' + type).appendTo(listIndicators);
         ++i;
     });
 }
@@ -34,4 +61,41 @@ $(document).ready(function () {
         var type = $(element).attr('data-meal-type');
         get_content(type);
      }).carousel({interval:false});
+
+
+//    $('#quote-carousel').carousel({interval:false});
 });
+//
+//
+//<div class="item active">
+//                    <div class="row pad-top-20">
+//                        <div class="col-xs-4 text-center recipe_pix">
+//                            <img class="img-circle" src="http://wp-desk.com/lovinflat/images/persons/person_1.png">
+//                        </div>
+//                        <div class="col-xs-8 item-info">
+//                            <p>Recipe Name</p>
+//                            <span class="label label-warning">Soups</span>
+//                            <span class="label label-warning">Main Dishes</span>
+//                            <span class="label label-warning">Italian</span>
+//                        </div>
+//                    </div>
+//                    <div class="row">
+//                        <div class="col-xs-6">
+//                            <div class="rating">
+//                                <span class="glyphicon glyphicon-star-empty"></span>
+//                                <span class="glyphicon glyphicon-star-empty"></span>
+//                                <span class="glyphicon glyphicon-star-empty"></span>
+//                                <span class="glyphicon glyphicon-star-empty"></span>
+//                                <span class="glyphicon glyphicon-star-empty"></span>
+//                            </div>
+//                        </div>
+//                        <div class="col-xs-6">
+//                            <div class="btn-group btn-group-xs pull-right">
+//                                <a class="btn btn-primary" href="#"><span class="fui-time"></span></a>
+//                                <a class="btn btn-primary" href="#"><span class="fui-check"></span></a>
+//                                <a class="btn btn-primary" href="#"><span class="fui-heart"></span></a>
+//                                <a class="btn btn-primary" href="#"><span class="fui-eye"></span></a>
+//                            </div>
+//                        </div>
+//                    </div>
+//                </div>
