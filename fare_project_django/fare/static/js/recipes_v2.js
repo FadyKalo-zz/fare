2509ffc5ddeb78aea97f55f0b06650b4027d2b74
Andreas Williams <aw2610@doc.ic.ac.uk>
2014-04-05 00:32:25 +0100

    updated UI in the meals page

--- fare_project_django/fare/static/js/recipes_v2.js
+++ fare_project_django/fare/static/js/recipes_v2.js
@@ -2,19 +2,14 @@ function get_content(type, diet) {
     $.get('/dietapp/recipe/' + type+"?diet="+diet, function () {
     }).done(function (data) {
             load_carousel(data, type);
-        })
-        .fail(function () {
-            console.log("error");
-        })
-        .always(function () {
-//            console.log("finished");
-        });
+    })
+    .fail(function () {
+        console.log("error");
+    });
 }
 
 function load_carousel(content, type) {
-    console.log(content);
     var carousel = $("#carousel-" + type).find('.carousel-inner');
-//    var listIndicators = $("#carousel-" + type + " ol.carousel-indicators");
     var i = 1;
     $.each(content, function (k, v) {
         $('<div>', {class: 'item'})
@@ -28,13 +23,11 @@ function load_carousel(content, type) {
 
                     )
         )
-        .append( $('<div>',{class:"row"}).append(
-                $('<div>',{class:"col-xs-5"}).append(
-                    $('<div>',{class:"rating"}).append(
-
-                    )
-                )
-            ).append($('<div>',{class:"col-xs-7"}).append( $('<div>',{class:"btn-group btn-group-xs pull-right"}
+        .append($('<div>',{class:"row"}).append(
+            $('<div>',{class:"col-xs-3 col-xs-offset-1"}).append(
+                $('<div>',{class:"rating"})
+            )
+            ).append($('<div>',{class:"col-xs-7"}).append( $('<div>',{class:"btn-group btn-group-sm"}
                 ).append(
                     $('<a>',{class:"btn btn-primary",href :'#' }).append(
                         $('<span>',{class:'fui-time'}).html(' '+v[2]/60 + 'min'))
@@ -53,24 +46,9 @@ function load_carousel(content, type) {
             )
         ).appendTo(carousel);
 
-        var rating = carousel.find('.item').last().find('.rating'); console.log(rating);
-        for(j=1;j<6;j++){
-            var rank = $('<span>', {class: 'glyphicon'});
-            if (j <= v[3]) {
-                $(rating).append(rank.addClass('glyphicon-star'));
-            } else {
-                $(rating).append(rank.addClass('glyphicon-star-empty'));
-            }
-        }
-        var arr = v[4].cuisine;
-        if(typeof arr != 'undefined'){
-            var info = carousel.find('.item').last().find('.item-info'); console.log(info);
-            for(j=0;j<arr.length;j++){
-                var label = $('<span>', {class: 'label'});
-                    $(info).append(label.html(arr[j]));
-            }
-        }
-
+        var rating = carousel.find('.item').last().find('.rating');
+        var rank = $('<span>', {class: 'glyphicon'});
+        $(rating).append($('<span>', {class: "pull-right"}).html('x ' + Math.round(v[3]))).append(rank.addClass('glyphicon-star'));
         ++i;
     });
 }
@@ -81,7 +59,4 @@ $(document).ready(function () {
         var diet = $(element).attr('data-diet');
         get_content(type, diet);
      }).carousel({interval:false});
-
-
-//    $('#quote-carousel').carousel({interval:false});
 });
